8ab835e3c431c420a2c88a31900e2b93bd28c01c
rivten <rivten@riseup.net>
2023-11-02 21:47:20 +0100

    2020 - day 02 - part 1

---
+++ 2020/02/Program.cs
@@ -0,0 +1,32 @@
+﻿
+static bool IsPasswordValid(string password, int smallest, int biggest, char ruleChar)
+{
+    int count = 0;
+    foreach(char c in password)
+    {
+        if (c == ruleChar)
+        {
+            count++;
+        }
+    }
+    return count >= smallest && count <= biggest;
+}
+
+string? line;
+int validCount = 0;
+
+while((line = Console.ReadLine()) != null)
+{
+    string[] policyPassword = line.Split(":");
+    string[] countsAndChar = policyPassword[0].Split(" ");
+    string[] counts = countsAndChar[0].Split("-");
+    string ruleChar = countsAndChar[1];
+    string password = policyPassword[1];
+    int smallest = Int32.Parse(counts[0]);
+    int biggest = Int32.Parse(counts[1]);
+    if(IsPasswordValid(password, smallest, biggest, ruleChar[0]))
+    {
+        validCount++;
+    }
+}
+Console.WriteLine("{0}", validCount);
