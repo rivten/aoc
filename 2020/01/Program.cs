ab8bbd75ad023479e55c92045c4b750d3ee99c26
rivten <rivten@riseup.net>
2023-11-02 19:04:49 +0100

    day 01 - part 1

---
+++ 2020/01/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string? line;
+
+List<int> report = new List<int>();
+while ((line = Console.ReadLine()) != null)
+{
+    report.Add(Int32.Parse(line));
+}
+
+foreach (int x in report)
+{
+    foreach (int y in report)
+    {
+        if (x + y == 2020)
+        {
+            Console.WriteLine("{0} {1}", x, y);
+            Console.WriteLine("{0}", x * y);
+        }
+    }
+}
